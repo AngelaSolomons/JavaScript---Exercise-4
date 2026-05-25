0c38b4338db4399c1d35689db0b9ae981bc4bca1
khanyagcilitshane-source <khanyagcilitshane@gmail.com>
2026-05-25 03:04:07 -0700

    Add files via upload
    Upload code

---
+++ conditional.js
@@ -0,0 +1,22 @@
+// Angela S created the variables and wrote the first if statement 
+// Create Variables for score and passing grade  
+let score = 100;
+let passingGrade = 60;
+
+// TODO: Write an if statement to check if score is greater than or equal to passingGrade 
+if (score >= passingGrade) {
+    console.log("You passed!");
+}
+
+
+// Khanya G created the else statement and the second if statement
+// TODO: Add an else statement for when the score is below passingGrade 
+else {
+    console.log("You failed.");
+}
+
+
+// TODO: Add another condition to check if the score is perfect (100) 
+if (score === 100) {
+    console.log("You passed with a perfect score!");
+}
